547bb82931bbe196d75413049543d4b99f4242dc
JamesPan <james14123@hotmail.com.tw>
2019-03-10 23:02:01 +0800

    make it better

--- index.js
+++ index.js
@@ -103,6 +103,85 @@ app.post('/gossiping', (appreq, appres) => {
 
 
 
+app.post('/beauty', (appreq, appres) => {
+	
+	var j = request.jar()
+		var cookie = request.cookie('over18=1');
+		var url = 'https://www.ptt.cc/bbs/beauty/index.html';
+		j.setCookie(cookie, url, function (err, cookie){});
+	
+	request({url: url, jar: j}, (err, res, body) => {
+		
+		post.length = 0;
+		list.length = 0;
+		content.length = 0;
+		
+		
+		var $ = cheerio.load(body)
+		// 抓取文章列表
+		list = $('.r-ent a').map((index, obj) => {
+			if ( $(obj).text().match('搜尋同標題文章') == null ){
+				if ( $(obj).text().match('搜尋看板內') == null ){
+					if ( $(obj).text().match('[公告]111') == null ){
+						if ( $(obj).text().match('[協尋]111') == null ){
+
+							return {
+							  title: $(obj).text(),
+							  link: 'https://www.ptt.cc' + $(obj).attr('href'),
+							}
+						}
+					}
+				}
+			}
+
+
+		}).get()
+
+		author = $('.author').map((index, obj) => {
+			return {
+				author: $(obj).text(),
+			}
+
+		}).get()
+
+		date = $('.date').map((index, obj) => {
+			return {
+				date: $(obj).text(),
+			}
+
+		}).get()
+
+		count = $('.r-ent .nrec').map((index, obj) => {
+					return {
+					  title: $(obj).text(),
+
+					}
+				}).get()
+
+
+		//console.log(count);
+
+		for(i=0;i<list.length;i++){
+
+			post.push({
+				title: list[i].title,
+				author:author[i].author,
+				date:date[i].date,
+				count: count[i].title, 
+				link: list[i].link,
+			})
+
+		}
+		
+	
+
+		appres.send(post);
+	});
+	
+
+	
+});
+
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
