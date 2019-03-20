5ff5b001c42cf64393a51c73ce405871613c6c25
JamesPan <james14123@hotmail.com.tw>
2019-03-20 21:31:53 +0800

    make it better

--- index.js
+++ index.js
@@ -341,6 +341,111 @@ app.post('/beauty', (appreq, appres) => {
 });
 
 
+
+app.post('/EAseries', (appreq, appres) => {
+	
+	var j = request.jar()
+		var cookie = request.cookie('over18=1');
+		var url = 'https://www.ptt.cc/bbs/EAseries/index.html';
+		j.setCookie(cookie, url, function (err, cookie){});
+	
+	request({url: url, jar: j}, (err, res, body) => {
+		
+		post.length = 0;
+		list.length = 0;
+		content.length = 0;
+		
+		
+		var $ = cheerio.load(body)
+		// 抓取文章列表
+		list = $('.r-ent a').map((index, obj) => {
+			if ( $(obj).text().match('搜尋同標題文章') == null ){
+				if ( $(obj).text().match('搜尋看板內') == null ){
+					if ( $(obj).text().match('[公告]111') == null ){
+						if ( $(obj).text().match('[協尋]111') == null ){
+
+							return {
+							  title: $(obj).text(),
+							  link: 'https://www.ptt.cc' + $(obj).attr('href'),
+							}
+						}
+					}
+				}
+			}
+
+
+		}).get()
+
+		author = $('.author').map((index, obj) => {
+			return {
+				author: $(obj).text(),
+			}
+
+		}).get()
+
+		date = $('.date').map((index, obj) => {
+			return {
+				date: $(obj).text(),
+			}
+
+		}).get()
+
+		count = $('.r-ent .nrec').map((index, obj) => {
+					return {
+					  title: $(obj).text(),
+
+					}
+				}).get()
+
+
+		//console.log(count);
+		let prevLink = '';
+		prev = $('.btn,.wide').map((index, obj) => {
+			if($(obj).text().includes('上頁')) {
+					return {
+					  prevLink: $(obj).attr('href'),
+
+					}
+				}
+			}).get()
+		
+	
+		function getPageNumber() {
+			let prev1 = prev[0].prevLink;
+			if (prev1 === '') return 1;
+			//console.log(prev);
+			if (!/index(\d*)\.html/.test(prev1)) return 'error';
+			let prevPageNumber = /index(\d*)\.html/.exec(prev1)[1];
+			return Number(prevPageNumber) + 1;
+		  }
+		var nowpage = getPageNumber();
+		
+
+		//console.log(count);
+
+		for(i=0;i<list.length;i++){
+
+			post.push({
+				title: list[i].title,
+				author:author[i].author,
+				date:date[i].date,
+				count: count[i].title, 
+				link: list[i].link,
+				nowpage: nowpage,
+			})
+
+		}
+		
+	
+
+		appres.send(post);
+	});
+	
+
+	
+});
+
+
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 app.post('/post',urlencodedParser, (appreq, appres) => {
