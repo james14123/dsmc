cc596a1c500c6917cde8ee1a015e0691729fc102
JamesPan <james14123@hotmail.com.tw>
2020-07-30 13:04:08 +0800

    make it better

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ var list = [];
 var post = [];
 var content= []; 
 
-app.post('/', (appreq, appres) => {
+app.post('/sex', (appreq, appres) => {
 	var url = "https://www.dcard.tw/_api/forums/sex/posts?popular=true";
 	//var url = "https://dcard.tw/_api/posts?popular=true";
 	request({url: url}, (err, res, body) => {
@@ -40,16 +40,62 @@ app.post('/', (appreq, appres) => {
 });
 
 
+app.post('/animal_crossing', (appreq, appres) => {
+	var url = "https://www.dcard.tw/_api/forums/animal_crossing/posts?popular=true";
+	//var url = "https://dcard.tw/_api/posts?popular=true";
+	request({url: url}, (err, res, body) => {
+		var $ = cheerio.load(body)
+		// 抓取文章列表
+		//list = $('article > h2 > a> span').map((index, obj) 
+		list = $('body').map((index, obj) => {
+			return {
+				Content: JSON.parse($(obj).text()),
+			}
+				
+		}).get()
+		
+		//console.log("Test: "+list);
+		appres.send(list[0].Content);
+	});
+	
+});
+
+
+app.post('/hotpost', (appreq, appres) => {
+	var url = "https://www.dcard.tw/_api/posts?popular=true";
+	//var url = "https://dcard.tw/_api/posts?popular=true";
+	request({url: url}, (err, res, body) => {
+		var $ = cheerio.load(body)
+		// 抓取文章列表
+		//list = $('article > h2 > a> span').map((index, obj) 
+		list = $('body').map((index, obj) => {
+			return {
+				Content: JSON.parse($(obj).text()),
+			}
+				
+		}).get()
+		
+		//console.log("Test: "+list);
+		appres.send(list[0].Content);
+	});
+	
+});
+
+
+
+
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 app.post('/post',urlencodedParser, (appreq, appres) => {
-	
+	var feedback = [];
 	var j = request.jar()
+	var j1 = request.jar()
 	var cookie = request.cookie('over18=1');
-	var url = appreq.body.name;
-	j.setCookie(cookie, url, function (err, cookie){});
+	var url = "https://www.dcard.tw/_api/posts/"+appreq.body.name;
+	var urlcomments = "https://www.dcard.tw/_api/posts/"+appreq.body.name+"/comments";
 	
+	j.setCookie(cookie, url, function (err, cookie){});
 	
 	request({url: url, jar: j}, (err, res, body) => {
 		
@@ -58,15 +104,43 @@ app.post('/post',urlencodedParser, (appreq, appres) => {
 				
 				return {
 					Content: $(obj).text(),
-
+					
 						
 				}
 			}).get()
 		
 		
 		
-		appres.send(JSON.parse(conlist[0].Content));
+		request({url: urlcomments, jar: j1}, (err, res, body) => {
+		
+		var $ = cheerio.load(body);
+		conlistcomments = $('body').map((index, obj) => {
+				
+				return {
+					commentsContent: $(obj).text(),
+
+						
+				}
+			}).get()
+			
+		feedback.push({
+			content1:JSON.parse(conlist[0].Content),
+			content2:JSON.parse(conlistcomments[0].commentsContent)
+		});
+				
+		appres.send(feedback[0]);
+			
+		feedback[0]="";	
+			
+		});
+		
+		
+		
+		
+		
 	});
+	
+
 	
 });
 
