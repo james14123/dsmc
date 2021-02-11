94acb5555c8907d8e917f67f0d83dc0f08e4a964
JamesPan <james14123@hotmail.com.tw>
2021-02-11 22:51:01 +0800

    make it better

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ var cheerio = require('cheerio');
 var cors = require('cors');
 
 
-app.use(cors());
+
 
 app.use(function (req, res, next) {
 
@@ -23,7 +23,7 @@ app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
   next();
 });
-
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true })); 
 
 
