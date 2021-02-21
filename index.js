71040b96241e670344bb3cf9732b6b695d337f24
JamesPan <james14123@hotmail.com.tw>
2021-02-21 22:11:58 +0800

    make it better

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ const corsOptions = {
   allowedHeaders: ['Content-Type', 'Authorization'],
 };
 
-app.use(cors(corsOptions));
+app.use(cors(allowCrossDomain));
 
 
 app.use(bodyParser.urlencoded({ extended: true })); 
